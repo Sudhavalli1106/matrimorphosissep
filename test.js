43c85007f97ffe6e03ba62681c83daaa0bbf1660
Sudhavalli1106 <sudhavalli.parthasarathy@matrimony.com>
2025-09-09 15:39:02 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 console.log("welcome to git part 2")
-let sessionName="Matrimorphosis"
+let sessionName="Matrimorphosis - batch 2"
 let topicName="resolving conflict"
 let duration="3 hours"
 let trainerName="Sudha"
 console.log(sessionName)
+console.log(topic Name)
