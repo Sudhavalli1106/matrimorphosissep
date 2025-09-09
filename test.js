3881545b9937f427fdcd07d2538004535680370b
sudhavalli <sudhavalli.parthasarathy@matrimony.com>
2025-09-09 15:36:56 +0530

    duration changed and session name printed

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 console.log("welcome to git part 2")
 let sessionName="Matrimorphosis"
 let topicName="resolving conflict"
-let duration="2 hours"
+let duration="3 hours"
 let trainerName="Sudha"
+console.log(sessionName)
